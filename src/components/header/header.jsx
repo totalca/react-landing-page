c23d54efb248b059c3e60eadcb2a53b04c1d4462
Nicolae Totalca <totalca@yahoo.com>
2018-11-06 21:49:03 +0000

    A few changes on header and background

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ class Header extends Component {
       <header>
         <Nav />
           <div className="top">
-              <h1>we design, <br/>you dream</h1>
+              <h1>we design <br/>your dream</h1>
               <div>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at nunc sed sapien faucibus molestie id quis tellus. Phasellus malesuada elit nunc, sed fermentum orci.</p>
                   <div><a href="#contact" className="contact">Contact Us</a></div>
