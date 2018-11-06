666558ad33d3a97ff02656b0514ebcbb12774c78
Nicolae Totalca <totalca@yahoo.com>
2018-11-06 17:01:57 +0000

    Header is almost finished

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './header.css';
 
 class Header extends Component {
   render() {
     return (
       <header>
-          <div className="header">
-              <h1>Landing Page,<br /> created with React</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at nunc sed sapien faucibus molestie id quis tellus. Phasellus malesuada elit nunc, sed fermentum orci.</p>
+          <div className="top">
+              <h1>A simple, <br/>Landing Page</h1>
+              <div>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at nunc sed sapien faucibus molestie id quis tellus. Phasellus malesuada elit nunc, sed fermentum orci.</p>
+                  <div><a href="#" className="contact">Contact Us</a></div>
+              </div>
           </div>
       </header>
     )
   }
 }
 
-export default Header
+export default Header;
