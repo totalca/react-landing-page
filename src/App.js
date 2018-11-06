785dd9467a06521dbbc3264d10a38fe1695b0fdd
Nicolae Totalca <totalca@yahoo.com>
2018-11-06 22:30:33 +0000

    Main created and background changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import Main from './components/main/main'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
           <Header />
+          <Main />
           <Footer />
       </div>
     );
---
+++ src/components/main/main.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './main.css'
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <section className="intro">
+          <h2>About Us</h2>
+          <div>
+            <p>Mauris vestibulum luctus dui. Vestibulum a nisl vel nisi malesuada maximus ut et nulla. Duis non facilisis est, bibendum consectetur sem. Etiam ut nunc ac magna ornare lobortis at sit amet lectus. Etiam eget leo tempor, condimentum turpis sit amet, ultricies nunc. Aliquam erat volutpat. Morbi a egestas arcu. Nullam ac facilisis urna. Morbi posuere semper lacus vel sagittis. Nam facilisis accumsan efficitur.</p>
+          </div>
+        </section>
+      </main>
+    )
+  }
+}
+
+export default Main;
