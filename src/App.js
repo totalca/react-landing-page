5ecd78c44297cc926e872d7d172a2b55320e6b74
Nicolae Totalca <totalca@yahoo.com>
2018-11-06 21:21:15 +0000

    Footer added and the screen has been resized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
           <Header />
+          <Footer />
       </div>
     );
   }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './footer.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <h3>CreativeLabs</h3>
+        <p>Support Center <br /> support@email.com</p>
+        <ul>
+          <li><a href="#facebook"><i className="fab fa-facebook-square"></i></a></li>
+          <li><a href="#twitter"><i className="fab fa-twitter-square"></i></a></li>
+          <li><a href="#instagram"><i className="fab fa-instagram"></i></a></li>
+        </ul>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
