f09a7a91a407d8886ce6eee6f804e587f3a059b1
Nicolae Totalca <totalca@yahoo.com>
2018-11-06 16:00:26 +0000

    Create header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+          <div className="header">
+              <h1>Landing Page,<br /> created with React</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at nunc sed sapien faucibus molestie id quis tellus. Phasellus malesuada elit nunc, sed fermentum orci.</p>
+          </div>
+      </header>
+    )
+  }
+}
+
+export default Header
