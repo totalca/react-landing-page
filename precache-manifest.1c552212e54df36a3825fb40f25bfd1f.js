43c837fb0fd4a27132a6ee8782e9a3bf036cc3e3
Nicolae Totalca <totalca@yahoo.com>
2018-11-08 19:20:51 +0000

    Updates

---
+++ precache-manifest.1c552212e54df36a3825fb40f25bfd1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e85b82c36f98b3438680c65d6c564cb",
+    "url": "/react-landing-page/static/media/gallery5.7e85b82c.jpeg"
+  },
+  {
+    "revision": "f430c93bf7b988099749",
+    "url": "/react-landing-page/static/css/main.4ba4d6db.chunk.css"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/react-landing-page/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "fef35db4759a96441639",
+    "url": "/react-landing-page/static/js/runtime~main.fef35db4.js"
+  },
+  {
+    "revision": "cdc8455b36604f471fc50a20ab0f6936",
+    "url": "/react-landing-page/static/media/background.cdc8455b.jpeg"
+  },
+  {
+    "revision": "a78840cd678d96e71bbc69934f4d08e4",
+    "url": "/react-landing-page/static/media/gallery6.a78840cd.jpeg"
+  },
+  {
+    "revision": "f430c93bf7b988099749",
+    "url": "/react-landing-page/static/js/main.f430c93b.chunk.js"
+  },
+  {
+    "revision": "66eb0b4e304d953fa6e8028a882f889d",
+    "url": "/react-landing-page/static/media/gallery2.66eb0b4e.jpeg"
+  },
+  {
+    "revision": "c06f0591c37cc408afc0d1c363f8d333",
+    "url": "/react-landing-page/static/media/gallery4.c06f0591.jpeg"
+  },
+  {
+    "revision": "2461fb7d28ee987a1def3699bb3c9365",
+    "url": "/react-landing-page/static/media/gallery3.2461fb7d.jpeg"
+  },
+  {
+    "revision": "4349d26f79fcca1352a1f440d71f18a9",
+    "url": "/react-landing-page/static/media/gallery1.4349d26f.jpeg"
+  },
+  {
+    "revision": "5670a68ef5c074feae155e17d16d2c69",
+    "url": "/react-landing-page/index.html"
+  }
+];
